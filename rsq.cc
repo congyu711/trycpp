0d664b7abefb0f65bd1df1a2a8d7571d4a167281
congyu711 <sxlxcsxlxc@gmail.com>
2024-02-16 20:35:37 +0800

    Fenwick tree == 树状数组

---
+++ rsq.cc
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+// https://codeforces.com/blog/entry/94227
+// https://oi-wiki.org/ds/fenwick/#%E5%8C%BA%E9%97%B4%E5%8A%A0%E5%8C%BA%E9%97%B4%E5%92%8C
+// Fenwick tree is "树状数组"...
+template <class T>
+struct rsq_add
+{
+    explicit rsq_add(size_t sz = 0) : f(sz) {}
+
+    rsq_add(const vector<auto> &vals) : f(size(vals))
+    {
+        for (size_t i = 0; i < size(vals); ++i)
+        {
+            T x = vals[i];
+            if (i)
+                x -= T(vals[i - 1]);
+            f[i].first += x;
+            f[i].second += x * T(i);
+            if (size_t j = i | (i + 1); j < size(f))
+            {
+                f[j].first += f[i].first;
+                f[j].second += f[i].second;
+            }
+        }
+    }
+
+    void add(size_t l, size_t r, const T &val)
+    {
+        if (l >= r)
+            return;
+        add_suf(l, val);
+        add_suf(r, -val);
+    }
+
+    T operator()(size_t l, size_t r) const
+    {
+        if (l >= r)
+            return T();
+        return sum_until(r) - sum_until(l);
+    }
+
+    inline T operator[](size_t i) const { return sum_until(i + 1) - sum_until(i); }
+
+private:
+    vector<pair<T, T>> f;
+
+    void add_suf(size_t pos, const T &val)
+    {
+        T m = val * T(pos);
+        for (size_t i = pos; i < size(f); i |= i + 1)
+            f[i].first += val, f[i].second += m;
+    }
+
+    T sum_until(size_t pos) const
+    {
+        T a{}, b{};
+        for (size_t i = pos; i--; i &= i + 1)
+            a += f[i].first, b += f[i].second;
+        return a * T(pos) - b;
+    }
+};
